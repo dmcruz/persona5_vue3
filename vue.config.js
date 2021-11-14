c96742d71f523e8c1090b6de28a5f2c5de9d2daf
Donna <dmcruz@gmail.com>
2021-11-15 01:05:57 +0800

    update Mythology; fix css

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/persona5_vue3/'
-    : '/',
+  publicPath: '/persona5_vue3/',
   css: {
     loaderOptions: {
       less: {
